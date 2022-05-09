9c6c3cb127561ad09db2e5e9473799764f11e685
Merichka <nik-meri@yandex.ru>
2022-05-09 19:58:32 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/model/User.java
@@ -0,0 +1,22 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+}
